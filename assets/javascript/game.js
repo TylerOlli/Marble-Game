ee1fec20560a05fdb57d6581e828f2dff0776f37
TylerOlli <ollityler@gmail.com>
2017-01-08 22:57:39 -0500

    Instructions 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -144,5 +144,5 @@ $("#marble4").click(function() {
 
 // Instructions
 $( "#instructions" ).click(function() {
-  alert( "Instructions: You will be given a random number at the start of the game. There are four marbles below. By clicking on a marble you will add a specific amount of points to your total score. You win the game by matching your total score to the random number, you lose the game if your total score goes above the random number. Each time when the game starts, the game will change the values of each crystal.");
+  alert( "Instructions: You will be given a random number at the start of the game. There are four marbles below. By clicking on a marble you will add a specific amount of points to your total score. You win the game by matching your total score to the random number, you lose the game if your total score goes above the random number. Each time when the game starts, the game will change the values of each marble.");
 });
